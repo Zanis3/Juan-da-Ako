8c3cc35ff3e11f7753d32ebf7ef738e8d75aa88e
JohnnyTopside-29 <manzanillomaveric@gmail.com>
2025-11-26 00:11:23 +0800

    q

---
+++ Assets/Scripts/Quiz/AnswerData.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+public class AnswerData : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] TextMeshProUGUI infoTextObject;
+    [SerializeField] UnityEngine.UI.Image toggle;
+    [Header("Textures")]
+    [SerializeField] Sprite uncheckedToggle;
+    [SerializeField] Sprite checkedToggle;
+    [Header("References")]
+    [SerializeField] QuizGameEvents events;
+
+    private RectTransform _rect;
+    public RectTransform Rect
+    {
+        get
+        {
+            if (_rect == null)
+            {
+                _rect = GetComponent<RectTransform>() ?? gameObject.AddComponent<RectTransform>();
+            }
+            return _rect;
+        }
+    }
+    private int _answerIndex = -1;
+    public int AnswerIndex {get {return _answerIndex;}}
+    private bool Checked = false;
+    public void UpdateData(string info, int index)
+    {
+        infoTextObject.text = info;
+        _answerIndex = index;
+    }
+    public void Reset()
+    {
+        Checked = false;
+        UpdateUI();
+    }
+    public void SwitchState()
+    {
+        Checked = !Checked;
+        UpdateUI();
+        if (events.UpdateQuestionAnswer != null)
+        {
+            events.UpdateQuestionAnswer(this);
+        }
+    }
+    void UpdateUI()
+    {
+        toggle.sprite = (Checked) ? checkedToggle : uncheckedToggle;
+    }
+}
---
+++ Assets/Scripts/Quiz/QuizGameEvents.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuizGameEvents", menuName = "Quiz/new GameEvents")] 
+public class QuizGameEvents : ScriptableObject
+{
+    public delegate void UpdateQuestionUICallback(Question question);
+    public UpdateQuestionUICallback UpdateQuestionUI;
+    public delegate void UpdateQuestionAnswerCallback(AnswerData pickedAnswer); 
+    public UpdateQuestionAnswerCallback UpdateQuestionAnswer;
+    public delegate void DisplayResolutionScreenCallback(QuizUIManager.ResolutionScreenType type, int score);
+    public DisplayResolutionScreenCallback DisplayResolutionScreen;
+    public delegate void ScoreUpdatedCallback();
+    public ScoreUpdatedCallback ScoreUpdated;
+    //[HideInInspector]
+    public int CurrentFinalScore;
+    [HideInInspector]
+    public int StartupHighscore;
+}
